6640f3d102d6273a454dfea98e49fbef48c0e4af
Anshul <kh.anshul04@gmail.com>
2024-10-16 09:19:54 +0530

    v2

--- src/utils/romImages.ts
+++ src/utils/romImages.ts
@@ -1,12 +1,12 @@
 const romimages: string[] = [
   "https://9to5google.com/wp-content/uploads/sites/4/2023/03/lineage-os-20-1.jpg?quality=82&strip=all",
   "https://image.jimcdn.com/app/cms/image/transf/none/path/s20617db5b5dfac4b/backgroundarea/i63acd836e804fdae/version/1580745873/image.jpg",
-  "https://static1.anpoimages.com/wordpress/wp-content/uploads/2021/10/pixel-6-pro-review-(3).jpg",
-  "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202205/android_13.jpg?versionid=qne_qi_mkp8t9mb5hdtbe_lqix2awo3w&size=690:388",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:and9gcrnrjqlzy2jirnjlg8n0twzgnoqwaeyaxepcq&s",
+  "https://avatars.githubusercontent.com/u/96394649?s=200&v=4",
+  "https://image.jimcdn.com/app/cms/image/transf/none/path/s20617db5b5dfac4b/backgroundarea/i63acd836e804fdae/version/1580745873/image.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0emA-S3dTUT2oRVPT5bL8tHrsLE1nOcDcIA&s",
   "https://images.boosty.to/image/9057bf50-b45b-4199-b432-b7e05320bc7e?change_time=1692370675",
-  "https://i.ytimg.com/vi/v43zmsbsc4w/maxresdefault.jpg",
-  "https://mi.fiime.cn/static/upload/2023/09/19/202309191694.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0emA-S3dTUT2oRVPT5bL8tHrsLE1nOcDcIA&s",
+  "https://androidsmart.github.io/img/rom/custom-roms.webp",
   "https://media.wired.com/photos/62d866d148e3599195fd2d08/1:1/w_1339,h_1339,c_limit/google-pixel-6a-review-fronts-gear.jpg",
   "https://afterlifeos.com/wp-content/uploads/2024/08/featured_home.png",
 ];
